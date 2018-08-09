984c6ed22385f11674b4f9211c33244efc8a2bd1
ushair <ushairansari@gmail.com>
2018-08-09 19:21:19 +0530

    done

---
+++ bfs.py
@@ -0,0 +1,47 @@
+import collections
+print("Breadth First Search :- ")
+graph = {'A': ['B', 'C', 'D'],
+         'B': ['A', 'F', 'E'],
+         'C': ['A'],
+         'D': ['A'],
+         'E': ['B'],
+         'F': ['B']
+         }
+
+
+# visits all the nodes of a graph (connected component) using BFS
+def bfs_connected_component(graph, start):
+    # keep track of all visited nodes
+    explored = []
+    # keep track of nodes to be checked
+    queue = [start]
+
+    levels = {}         # this dict keeps track of levels
+    levels[start] = 0    # depth of start node is 0
+
+    visited = [start]  # to avoid inserting the same node twice into the queue
+
+    # keep looping until there are nodes still to be checked
+    while queue:  # pop shallowest node (first node) from queue
+        node = queue.pop(0)
+        explored.append(node)
+        neighbours = graph[node]
+
+        # add neighbours of node to queue
+        for neighbour in neighbours:
+            if neighbour not in visited:
+                queue.append(neighbour)
+                visited.append(neighbour)
+
+                levels[neighbour] = levels[node]+1
+                print(neighbour, ">>", levels[neighbour])
+                if neighbour is 'F':
+                    print("goal found "+neighbour)
+                    break
+
+    print(levels)
+
+    return explored
+
+
+ans = bfs_connected_component(graph, 'A')
