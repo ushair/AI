6ba5a8948cfe5e6679594f1a7ccea1ec1a14b1ab
ushair <ushairansari@gmail.com>
2018-08-09 20:04:56 +0530

    done

--- dfs.py
+++ dfs.py
@@ -1,1 +1,24 @@
 print("DEPTH FIRST SEARCH")
+graph = {'A': ['B', 'C', 'D'],
+         'B': ['A', 'E', 'F'],
+         'C': ['A'],
+         'D': ['A'],
+         'E': ['B'],
+         'F': ['B']
+         }
+
+
+def dfs(graph, start):
+    path = []
+    stack = [start]
+    while stack != []:
+        v = stack.pop()
+        if v not in path:
+            path.append(v)
+        for w in reversed(graph[v]):
+            if w not in path:
+                stack.append(w)
+    return path
+
+
+print(dfs(graph, 'A'))
