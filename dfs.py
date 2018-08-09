7b4d000016f9bbc32b6bdce51bb85248825a6b62
ushair <ushairansari@gmail.com>
2018-08-09 20:29:57 +0530

    done

--- dfs.py
+++ dfs.py
@@ -8,23 +8,24 @@ graph = {'A': ['B', 'C', 'D'],
          }
 
 
-def dfs(graph, start):
+def dfs(graph, start, goal):
     levels = {}
     levels[start] = 0
-
     path = []
     stack = [start]
     while stack != []:
         v = stack.pop()
         if v not in path:
             path.append(v)
+        if v is goal:
+            print("GOAL FOUND :- " + v)
+            break
         for w in reversed(graph[v]):
             if w not in path:
                 levels[w] = levels[v]+1
                 stack.append(w)
-
     print(levels)
     return path
 
 
-print(dfs(graph, 'A'))
+print(dfs(graph, 'A', 'F'))
